19c1518a0e46ad90f6b6c75deaa309068d19063f
frostRepo <frost084@gmail.com>
2016-04-27 04:02:30 -0500

    diagnostico

--- build/scripts/diagnostic.js
+++ build/scripts/diagnostic.js
@@ -9,6 +9,7 @@
     var _dgHeader,
         _dgBody,
         _window,
+        _document,
         _sectionHeader,
         _dgOptions,
         _dgInit,
@@ -23,19 +24,25 @@
         _dgService,
         _dgProgressBar,
         _dgProgressInfo,
-        _dgGeneralOptions;
+        _dgGeneralOptions,
+        _dgNext,
+        _dgPrev,
+        _lengthQuestions,
+        _dgQuestionStart,
+        _activeOptionData,
+        _activeOptions;
 
     var _cache = function (header, body) {
       _dgStateCount = null;
       _dgHeader = header;
       _dgBody = body;
       _window = jq(window);
+      _document = jq(document)
       _sectionHeader = jq('.ia-ContentHeader');
-      _dgOptions = jq('.ia-Diagnostic__option');
       _dgInit = jq('#diagnostic-init');
       _dgGeneral = jq('#diagnostic-general');
       _dgStart = jq('.ia-Diagnostic__start');
-      _dgSession = {};
+      _dgSession = JSON.parse(sessionStorage.getItem('diagnostic')) || {};
       _dgContainer = jq('.ia-Content--diagnostic');
       _dgReset = jq('.ia-Diagnostic__reset');
       _dgQuestion = jq('.ia-Diagnostic__qst');
@@ -43,21 +50,26 @@
       _dgProgressBar = jq('.ia-Diagnostic__padding');
       _dgProgressInfo = jq('.ia-Diagnostic__count');
       _dgGeneralOptions = _dgGeneral.find('.ia-Diagnostic__options');
-
-      console.log(_dgService[1].general[0]);
+      _dgNext = jq('.ia-Diagnostic__ctrl--next');
+      _dgPrev = jq('.ia-Diagnostic__ctrl--prev');
     }
 
     var _bindEvents = function () {
+
       _window.resize(function () {
         _bodyRender();
+        _renderOptions();
       });
 
-      _dgOptions.on('click', function () {
+      _dgGeneral.on('click', '.ia-Diagnostic__option', function () {
         var el = jq(this);
         _optionSelected(el);
+        _validateControls();
       });
 
       _dgInit.on('click', '.ia-Diagnostic__option', function () {
+        var el = jq(this);
+        _optionSelected(el);
         _activeStart();
       });
 
@@ -68,21 +80,32 @@
       _dgReset.on('click', function () {
         _resetAction();
       });
+
+      _dgNext.on('click', function (evt) {
+        evt.preventDefault();
+        _nextAction();
+      });
+
+      _dgPrev.on('click', function (evt) {
+        evt.preventDefault();
+        _prevAction();
+      });
     }
 
     var _render = function () {
 
       if (_dgStateCount === null) {
-        _dgQuestion.text(_dgService[0].init.question)
+        _dgQuestion.text(_dgService[0].init.question);
       } else {
 
-        var _lengthQuestions = _dgService[1].general.length - _dgQuestionCount,
-            _porcent = (100 / _lengthQuestions) + '%',
+        _lengthQuestions = _dgService[1].general.length - _dgQuestionStart;
+
+        var _currentQuestion = _dgQuestionStart === 0
+              ? _dgQuestionCount + 1
+              : _dgQuestionCount,
+            _porcent = (100 / _lengthQuestions) * _currentQuestion + '%',
             _options = _dgService[1].general[_dgQuestionCount].options,
-            _blocks = '',
-            _currentQuestion = _dgQuestionCount === 0
-              ? 1
-              : _dgQuestionCount;
+            _blocks = '';
 
         _dgQuestion.text(_dgService[1].general[_dgQuestionCount].question);
 
@@ -92,17 +115,102 @@
           .text(_currentQuestion + '/' + _lengthQuestions)
           .css({'left' : _porcent});
 
-        console.log(typeof _options);
+        for (var i = 0; i < _options.length; i++) {
+          _blocks += '<div class="ia-Diagnostic__option" data-option="' + _options[i].letter + '"><div class="ia-Diagnostic__option__inner"><span class="ia-Diagnostic__option__letter" data-option="A">' + _options[i].letter + ' .</span><p class="ia-Diagnostic__option__copy">' + _options[i].copy + '</p></div></div>'
+        }
 
-        // _options.each(function (index, value) {
-        //
-        //   _blocks += '<div class="ia-Diagnostic__option"><div class="ia-Diagnostic__option__inner"><span class="ia-Diagnostic__option__letter" data-option="A">' + value.letter + ' .</span><p class="ia-Diagnostic__option__copy">' + value.copy + '</p></div></div>'
-        //
-        // });
+        _dgGeneralOptions.html(_blocks)
 
-        // _dgGeneralOptions.html(_blocks)
+        _renderOptions();
       }
 
+      _dgOptions = jq('.ia-Diagnostic__option');
+
+
+
+      _resetOptions();
+
+      _validateControls();
+
+    }
+
+    var _renderOptions = function () {
+      var _elements = _dgGeneral.find('.ia-Diagnostic__option .ia-Diagnostic__option__copy');
+
+      _elements = jq.map(_elements, function (val) {
+        return jq(val).innerHeight();
+      }).sort(function (a, b) {
+        return a < b;
+      });
+
+      if (_elements[0] > 156) {
+        _dgGeneral.find('.ia-Diagnostic__option').css({'height':_elements[0] + 64});
+      }
+    }
+
+    var _nextAction = function () {
+
+      if (_dgQuestionCount <= _lengthQuestions - 1) {
+        _sessionControlAdd('next');
+        _render();
+
+        _selectedCache();
+
+      }
+    }
+
+    var _prevAction = function () {
+      _sessionControlAdd('prev');
+      _render();
+      _selectedCache();
+    }
+
+    var _selectedCache = function () {
+      var _session = JSON.parse(sessionStorage.getItem('diagnostic'));
+
+      if (_session[String(_dgQuestionCount)]) {
+
+        var _selected = _session[String(_dgQuestionCount)];
+
+        _dgGeneral.find('.ia-Diagnostic__option[data-option="' + _selected + '"]').trigger('click');
+
+        _validateControls();
+      }
+    }
+
+    var _sessionControlAdd = function (action) {
+      _dgSession[String(_dgQuestionCount)] = _activeOptionData;
+      _dgQuestionCount = action === 'next'
+        ? _dgQuestionCount + 1
+        : _dgQuestionCount - 1;
+      _manageStorage('add');
+    }
+
+    var _validateControls = function () {
+
+      _activeOptions = jq('.js-active-option');
+
+      if (!_activeOptions.length) {
+        _dgNext.addClass('js-disabled');
+      } else {
+        _dgNext.removeClass('js-disabled')
+      }
+
+      if (_dgQuestionCount === _dgQuestionStart) {
+        _dgPrev.addClass('js-disabled');
+      } else {
+        _dgPrev.removeClass('js-disabled')
+      }
+
+    }
+
+    var _resetOptions = function () {
+      _dgOptions.removeClass('js-active-option');
+    }
+
+    var _resetProgress = function () {
+      _dgProgressBar.removeAttr('style');
+      _dgProgressInfo.removeAttr('style');
     }
 
     var _resetAction = function () {
@@ -110,16 +218,21 @@
       _toggleContainer();
       _manageStorage('kill');
       _dgStateCount = null;
+      _disableStart();
+      _resetProgress();
       _render();
+      _resetOptions();
+      _dgSession = {};
     }
 
     var _startAction = function () {
-      var _activeOptionData = jq('.js-active-option').data('option');
       _dgSession['init'] = _activeOptionData;
       _toggleContainer();
       _toggleStates();
 
       _validateGenere(_activeOptionData);
+
+      _dgQuestionCount = _dgQuestionStart;
 
       _manageStorage('add');
       _render();
@@ -127,7 +240,7 @@
 
     var _validateGenere = function (selected) {
       _dgStateCount = 1;
-      _dgQuestionCount = selected === 'M'
+      _dgQuestionStart = selected === 'M'
         ? 0
         : 1;
     }
@@ -154,10 +267,14 @@
       var _selections = JSON.parse(sessionStorage.getItem('diagnostic'));
 
       if (_session) {
+        _dgQuestionCount = parseInt(_session);
         _toggleContainer();
         _toggleStates();
         _validateGenere(_selections.init);
-        _render();
+
+        setTimeout(function () {
+          _selectedCache();
+        }, 100);
       }
     }
 
@@ -181,6 +298,7 @@
     var _optionSelected = function (el) {
       _dgOptions.removeClass('js-active-option');
       el.addClass('js-active-option');
+      _activeOptionData = jq('.js-active-option').data('option');
     }
 
     var _bodyRender = function () {
@@ -190,7 +308,7 @@
           : 102;
         var _bodyHeight = _window.innerHeight() - (_sectionHeader.innerHeight() + _fixedHeight)
 
-        _dgBody.css({'height':_bodyHeight});
+        _dgBody.css({'min-height':_bodyHeight});
       } else {
         _dgBody.removeAttr('style');
       }
@@ -201,11 +319,8 @@
       _cache(header, body)
       _initRender();
       _bodyRender();
+      _render();
       _bindEvents();
-
-      if (!_dgStateCount) {
-        _render()
-      }
     }
 
     /*Revealing*/
--- build/scripts/diagnostic.service.js
+++ build/scripts/diagnostic.service.js
@@ -65,6 +65,214 @@
                 }
               ]
             },
+            {
+              'id' : 2,
+              'question' : '¿Cuáles de estas descripciones crees que definirían tu cabello?',
+              'options' : [
+                {
+                  'letter' : 'A',
+                  'copy' : 'Abundante y espeso, aunque te cuesta trabajo moldearlo',
+                  'return' : 'No cepilles muy fuerte tu cabello, utiliza accesorios de cerdas muy suaves y procura peinarlo cuando esté seco, no mojado, ya que esto facilita que el cabello delgado se quiebre. <br> Evita todo lo posible el uso de secadores de cabello; si no tienes opción, utiliza las temperaturas más bajas.'
+                },
+                {
+                  'letter' : 'B',
+                  'copy' : 'Abundante pero escurridizo, los peinados se te vienen abajo a las pocas horas y pareciera que en realidad tienes poco cabello',
+                  'return' : 'No cepilles muy fuerte tu cabello, utiliza accesorios de cerdas muy suaves y procura peinarlo cuando esté seco, no mojado, ya que esto facilita que el cabello delgado se quiebre. <br> Evita todo lo posible el uso de secadores de cabello; si no tienes opción, utiliza las temperaturas más bajas.'
+                },
+                {
+                  'letter' : 'C',
+                  'copy' : 'Cualquiera de las dos anteriores pero la caída del cabello cada vez es más notoria a simple vista',
+                  'return' : 'No cepilles muy fuerte tu cabello, utiliza accesorios de cerdas muy suaves y procura peinarlo cuando esté seco, no mojado, ya que esto facilita que el cabello delgado se quiebre. <br> Evita todo lo posible el uso de secadores de cabello; si no tienes opción, utiliza las temperaturas más bajas.'
+                },
+                {
+                  'letter' : 'D',
+                  'copy' : 'Cuando pasas la mano por tu cabello, sientes las yemas de tus dedos como grasosos',
+                  'return' : 'No cepilles muy fuerte tu cabello, utiliza accesorios de cerdas muy suaves y procura peinarlo cuando esté seco, no mojado, ya que esto facilita que el cabello delgado se quiebre. <br> Evita todo lo posible el uso de secadores de cabello; si no tienes opción, utiliza las temperaturas más bajas.'
+                }
+              ]
+            },
+            {
+              'id' : 3,
+              'question' : '¿Qué tan visible es tu caída del cabello?',
+              'options' : [
+                {
+                  'letter' : 'A',
+                  'copy' : 'Sólo se cae, pero a simple vista la abundancia del cabello se ve normal.',
+                  'return' : false
+                },
+                {
+                  'letter' : 'B',
+                  'copy' : 'Una ligera circunferencia al centro del cráneo (que va haciéndose cada vez más notorio).',
+                  'return' : false
+                },
+                {
+                  'letter' : 'C',
+                  'copy' : 'Pronunciado en las entradas (en forma de M).',
+                  'return' : false
+                },
+                {
+                  'letter' : 'D',
+                  'copy' : 'Desde las sienes y hacia atrás (en forma de U).',
+                  'return' : false
+                }
+              ]
+            },
+            {
+              'id' : 4,
+              'question' : '¿Cuántas veces a la semana usas shampoo?',
+              'options' : [
+                {
+                  'letter' : 'A',
+                  'copy' : '1 o 2 ',
+                  'return' : 'Te recomendamos lavar tu cabello con más frecuencia. No lavarlo diario, provocará que los residuos que en él se albergan, sobretodo aquellos que se obtienen por factores externos, permanezcan en tu cabello, perjudicándolo con el paso del tiempo y favoreciendo su caída. '
+                },
+                {
+                  'letter' : 'B',
+                  'copy' : '3 a 5',
+                  'return' : 'Recomendación: con un poco de fuerza de voluntad podrías esforzarte un poco más para lavarte el cabello prácticamente diario. '
+                },
+                {
+                  'letter' : 'C',
+                  'copy' : '5 a 7',
+                  'return' : 'Es hora de que elijas muy bien los productos con los que lavas tu cabello. Presta mucha atención a los agentes con los que están hechos, ya que algunos dañan sobretodo el cuero cabelludo, irritándolo casi siempre y provocando la caída del cabello; asegúrate que contengan los nutrientes básicos como una buena gama de vitaminas B, además de nutrientes como el Pantenol, que le darán a tu cabello brillo natural y lo fortalecerán para evitar futuras caídas. '
+                },
+                {
+                  'letter' : 'D',
+                  'copy' : 'Más de 7',
+                  'return' : 'Es hora de que elijas muy bien los productos con los que lavas tu cabello. Presta mucha atención a los agentes con los que están hechos, ya que algunos dañan sobretodo el cuero cabelludo, irritándolo casi siempre y provocando la caída del cabello; asegúrate que contengan los nutrientes básicos como una buena gama de vitaminas B, además de nutrientes como el Pantenol, que le darán a tu cabello brillo natural y lo fortalecerán para evitar futuras caídas. '
+                }
+              ]
+            },
+            {
+              'id' : 5,
+              'question' : '¿A cuáles de estos factores expones mayoritariamente tu cabello?',
+              'options' : [
+                {
+                  'letter' : 'A',
+                  'copy' : 'Agua muy caliente.',
+                  'return' : 'Utiliza agua, de templada a fría, para lavarte el cabello. De acuerdo con la Asociación Americana para la Piel de los Estados Unidos, lavar el cabello con agua caliente o muy caliente, ocasiona que quede demasiado duro, lo cual se vuelve complicado de peinar. Además, las temperaturas altas suelen acentuar ciertos padecimientos, por lo que si tienes algún problema, tales como exceso de grasa, deshidratación o caspa, lo más probable es que notes más grasa o caspa si usas agua caliente. Lo ideal es que al lavar tu cabello con shampoo, utilices agua templada y, de usar enjuague, es recomendable el agua de templada a fría. Con esto no sólo mantendrás tu cabello a salvo, sino que notarás como su sedosidad y brillo natural cobran más fuerza.'
+                },
+                {
+                  'letter' : 'B',
+                  'copy' : 'Cualquier tipo de geles moldeadores del cabello, lacas o procesos químicos, tinte pare el cabello y/o calor.',
+                  'return' : 'Los tintes, sprays y secadoras afectan el ciclo normal del cabello. Por lo mismo, debes tener especial cuidado en los productos que utilizas, revisar muy bien sus componentes para asegurarte que no contengan químicos muy agresivos y evitar, en la medida de lo posible, que lleguen a la raíz. <br> Productos con vitaminas y sustancias activas como Fintantriol y Queratina ayudan a fortalecer el cabello después de exponerlo a tratamientos como tintes, permanentes o secadoras, evitando su caída.'
+                },
+                {
+                  'letter' : 'C',
+                  'copy' : 'Excesiva sudoración. Estrés.',
+                  'return' : false
+                },
+                {
+                  'letter' : 'D',
+                  'copy' : 'Smog, cloro de alberca, contaminantes extremos.',
+                  'return' : 'La Sociedad Iberoamericana de Dermatología señala que: "El humo, la contaminación y la suciedad urbana se pueden acumular en el cabello, dándole un aspecto mate y desvitalizado que puede generar una caída prematura. Asimismo, el humo del tabaco y del gas natural de las cocinas, contaminantes externos, etcétera, también puede tener como efecto secundario la decoloración del cabello". Desgraciadamente es difícil controlar estos factores puesto que quedarse encerrado no es una opción. Una buena forma de mantenerlo a salvo es usando enjuague además del shampoo, de preferencia que el enjuague tenga propiedades hidratantes para mayor protección. <br> Si entras al mar y, sobre todo, a albercas, en la medida de lo posible, trata de lavar tu cabello con agua dulce y/o agua natural para eliminar sales y el cloro de las piscinas que irritan el cuero cabelludo, desgastándolo. También puedes usar un gorro de goma.'
+                }
+              ]
+            },
+            {
+              'id' : 6,
+              'question' : '¿Cómo describirías tu nivel de estrés?',
+              'options' : [
+                {
+                  'letter' : 'A',
+                  'copy' : 'A decir verdad, siempre estoy bastante relajado.',
+                  'return' : false
+                },
+                {
+                  'letter' : 'B',
+                  'copy' : 'Bajo, sólo te estresas con situaciones realmente extremas, quizás una vez a la semana.',
+                  'return' : '¡Cuidado con el estrés! Caminar por un periodo de al menos 10 minutos un par de veces al día te ayudará a disminuir tus niveles de estrés. Reducir tu consumo de cafeína también te ayudará a controlarlo. Verifica que estás cumpliendo con tus horas de sueño necesarias.'
+                },
+                {
+                  'letter' : 'C',
+                  'copy' : 'Sueles estresarte varias veces a la semana y el nivel aumenta cuando hay exámenes o complicaciones en la oficina.',
+                  'return' : '¡Cuidado con el estrés! Caminar por un periodo de al menos 10 minutos un par de veces al día te ayudará a disminuir tus niveles de estrés. Reducir tu consumo de cafeína también te ayudará a controlarlo. Verifica que estás cumpliendo con tus horas de sueño necesarias.'
+                },
+                {
+                  'letter' : 'D',
+                  'copy' : 'Te estresas ante cualquier situación sin pensarlo, varias veces al día.',
+                  'return' : '¡Cuidado con el estrés! Caminar por un periodo de al menos 10 minutos un par de veces al día te ayudará a disminuir tus niveles de estrés. Reducir tu consumo de cafeína también te ayudará a controlarlo. Verifica que estás cumpliendo con tus horas de sueño necesarias.'
+                }
+              ]
+            },
+            {
+              'id' : 7,
+              'question' : '¿Cómo calificarías tus niveles de ingesta de alcohol?',
+              'options' : [
+                {
+                  'letter' : 'A',
+                  'copy' : 'No consumo alcohol.',
+                  'return' : false
+                },
+                {
+                  'letter' : 'B',
+                  'copy' : 'Los fines de semana.',
+                  'return' : 'Debes considerar seriamente bajar tus niveles de ingesta de alcohol y alimentos con altos contenidos de azúcares. Datos de la de la Biblioteca Nacional de Medicina de los Estados Unidos indican que el consumo del alcohol, en exceso, provoca una descompensación en la llamada bomba sodio-potasio del cuerpo, lo que agota las reservas de vitaminas y proteínas; esto es, muchas vitaminas y nutrientes no llegan a tu cabello como deberían y por ende se debilita y se cae.'
+                },
+                {
+                  'letter' : 'C',
+                  'copy' : '3 o 4 días por semana.',
+                  'return' : 'Debes considerar seriamente bajar tus niveles de ingesta de alcohol y alimentos con altos contenidos de azúcares. Datos de la de la Biblioteca Nacional de Medicina de los Estados Unidos indican que el consumo del alcohol, en exceso, provoca una descompensación en la llamada bomba sodio-potasio del cuerpo, lo que agota las reservas de vitaminas y proteínas; esto es, muchas vitaminas y nutrientes no llegan a tu cabello como deberían y por ende se debilita y se cae. '
+                },
+                {
+                  'letter' : 'D',
+                  'copy' : 'Más de 4 veces por semana.',
+                  'return' : 'Debes considerar seriamente bajar tus niveles de ingesta de alcohol y alimentos con altos contenidos de azúcares. Datos de la de la Biblioteca Nacional de Medicina de los Estados Unidos indican que el consumo del alcohol, en exceso, provoca una descompensación en la llamada bomba sodio-potasio del cuerpo, lo que agota las reservas de vitaminas y proteínas; esto es, muchas vitaminas y nutrientes no llegan a tu cabello como deberían y por ende se debilita y se cae. '
+                }
+              ]
+            },
+            {
+              'id' : 8,
+              'question' : '¿Cómo calificarías tus niveles de tabaco?',
+              'options' : [
+                {
+                  'letter' : 'A',
+                  'copy' : 'No fumo.',
+                  'return' : false
+                },
+                {
+                  'letter' : 'B',
+                  'copy' : 'Una cajetilla me dura más de una semana.',
+                  'return' : 'Trata de abandonar el hábito de fumar o reducir el número de cigarrillos, al menos por un tiempo, en lo que te concentras en fortalecer tu cabello. De acuerdo con la Academia América de Dermatología, los químicos que entran a tu cuerpo con cada bocanada de tabaco afectan la microcirculación del cuero cabelludo; la nicotina suele impedir que los nutrientes lleguen al cabello, lo que a su vez provoca resequedad extrema que finalmente favorece la caída.'
+                },
+                {
+                  'letter' : 'C',
+                  'copy' : 'Una cajetilla me dura menos de una semana.',
+                  'return' : 'Trata de abandonar el hábito de fumar o reducir el número de cigarrillos, al menos por un tiempo, en lo que te concentras en fortalecer tu cabello. De acuerdo con la Academia América de Dermatología, los químicos que entran a tu cuerpo con cada bocanada de tabaco afectan la microcirculación del cuero cabelludo; la nicotina suele impedir que los nutrientes lleguen al cabello, lo que a su vez provoca resequedad extrema que finalmente favorece la caída.'
+                },
+                {
+                  'letter' : 'D',
+                  'copy' : 'Más de una cajetilla a la semana.',
+                  'return' : 'Trata de abandonar el hábito de fumar o reducir el número de cigarrillos, al menos por un tiempo, en lo que te concentras en fortalecer tu cabello. De acuerdo con la Academia América de Dermatología, los químicos que entran a tu cuerpo con cada bocanada de tabaco afectan la microcirculación del cuero cabelludo; la nicotina suele impedir que los nutrientes lleguen al cabello, lo que a su vez provoca resequedad extrema que finalmente favorece la caída.'
+                }
+              ]
+            },
+            {
+              'id' : 9,
+              'question' : '¿Qué prioridad quisieras solucionar? ',
+              'options' : [
+                {
+                  'letter' : 'A',
+                  'copy' : 'Mucho antes de padecer una fuerte caída de cabello, te gustaría prevenirlo. Estás más preocupado por darle un sano volumen pero, sobretodo, moldearlo con facilidad y que el peinado te dure a lo largo del día.',
+                  'return' : 'Como todavía no tienes un indicio fuerte de pérdida de cabello, lo más recomendado para ti es utilizar cualquier producto Folicuré® Original que surge efecto desde la raíz, y con la que fortalecerás tu cabello. Además, vigorizarás el volumen y brillo natural de tu cabello, dándole un aspecto extremadamente sano; estarás previniendo una posible tendencia de caída de cabello si tienes antecedentes.'
+                },
+                {
+                  'letter' : 'B',
+                  'copy' : 'Tienes miedo de que tu cabello delgado tenga que ver algo con la caída del cabello; buscas una forma de fortalecer tu cabello.',
+                  'return' : 'Lo más probable, entre otros factores, es que tengas un cabello delgado y fino y éste tiende a ser quebradizo; de no tratarlo adecuadamente, puede ser un factor que propicie la caída del cabello ligeramente más rápido de lo normal. Muchos de los hombres con este tipo de cabello tienden a perderlo con mayor facilidad. Por eso, es recomendable que empieces a utilizar cualquier producto de Folicuré® Hidratante, así podrás mantener tu frágil cabello en buen estado. Con los ingredientes exclusivos de Folicuré® estarás previniendo o incluso revirtiendo los efectos de la caída del cabello.'
+                },
+                {
+                  'letter' : 'C',
+                  'copy' : 'Estás preocupado porque, además de la caída del cabello, crees que puedas estar padeciendo de algún problema de caspa.',
+                  'return' : 'Ciertos aspectos de tu estilo de vida están ocasionándote problemas de caspa y, aunque creas que por el momento esa una prioridad, algunos dermatólogos sugieren que también la caspa puede ser un factor que fomente la caída del cabello. Cualquier producto Folicuré® Control de Caspa te ayudará a solucionar los problemas con todo y sus molestos síntomas; al mismo tiempo, estarás nutriendo a tu cabello con los ingredientes  exclusivos de Folicuré®, con los que podrás prevenir la caída a tiempo.'
+                },
+                {
+                  'letter' : 'D',
+                  'copy' : 'Definitivamente, sientes que estás a contra reloj en la caída del cabello para no quedarte calvo.',
+                  'return' : 'Quizás te encuentres en un punto de caída del cabello progresiva, la línea Extra, en la que cada integrante además de contar con los nutrientes exclusivos de Folicuré® como los Folisacáridos y el Pantenol, está adicionado con Fintantriol y Queratina que dejan tu cabello manejable y sedoso. Cabello fuerte y brillante.'
+                }
+              ]
+            }
           ]
         }
     ];
